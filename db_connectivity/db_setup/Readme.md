1cdcedb86abdf79f330c6d0ee113598065df2e74
swapnilwagh <swapnilwagh@swapnilwaghs-MacBook-Air.local>
2023-11-05 20:31:08 -0800

    code restructured

--- db_connectivity/db_setup/Readme.md
+++ db_connectivity/db_setup/Readme.md
@@ -1,4 +1,4 @@
-   .env will contain the following:
+.env will contain the following:
 
       MONGO_INITDB_ROOT_USERNAME=
       MONGO_INITDB_ROOT_PASSWORD=
@@ -8,31 +8,11 @@
 **Ensure that the .env file is readable only by the user who will run the script and is not accessible by others, as it contains sensitive information**
 
       1.install MongoDB using docker -
-      
+
       build docker image first:
-      
+
       docker build -t mongodb-with-shell .
-      
+
       2.run mongodb server using docker container:
-      
+
       docker-compose up
-
-**cron script setting**   
-
-      1. Give the necessary permissions:
-      
-            chmod +x backup_script.sh
-      
-      2. Schedule the Backup with Cron
-   
-         Open the crontab configuration file:
-      
-            crontab -e
-
-      
-      Add the following line to schedule the script to run every Monday at 12 PM:
-      
-          MAILTO="your@email.com"
-          0 12 \* \* 1 /path/to/backup_mongodb.sh
-      
-      This cron job will execute the backup_mongodb.sh script every Monday at 12:00 PM. it will also mail once this cron execution done.
--- db_connectivity/test.py
+++ db_connectivity/test.py
@@ -6,32 +6,42 @@ from dotenv import load_dotenv
 def main():
     # Load environment variables from .env file
     load_dotenv()
-
     # MongoDB connection string
     db_user = os.getenv('DB_USER')
     db_pass = os.getenv('DB_PASS')
     db_host = os.getenv('DB_HOST', 'localhost')
     db_port = os.getenv('DB_PORT', '27017')
-    db_name = os.getenv('DB_NAME', 'mydatabase')
-    collection_name = os.getenv('COLLECTION_NAME', 'mycollection')
-
+    db_name = os.getenv('DB_NAME', 'sample_database')
+    collection_name = os.getenv('COLLECTION_NAME', 'users')
     connection_string = (
         f"mongodb://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
         "?authSource=admin"
     )
-
     # Initialize MongoDB client
     client = MongoClient(connection_string)
-
     # Select your database
     db = client[db_name]
-
     # Select your collection
     collection = db[collection_name]
 
+    # Insert the sample document
+    sample_data = {
+        "name": "John Doe",
+        "age": 30,
+        "address": {
+            "street": "123 Elm Street",
+            "city": "Springfield",
+            "zip": "12345"
+        },
+        "email": "johndoe@example.com",
+        "is_active": True,
+        "scores": [70, 88, 92]
+    }
+    insert_result = collection.insert_one(sample_data)
+    print(f"Inserted document id: {insert_result.inserted_id}")
+
     # Retrieve all documents from the collection
     documents = collection.find()
-
     # Print all documents
     for document in documents:
         print(document)
